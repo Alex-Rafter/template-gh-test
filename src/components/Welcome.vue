4046040b19272d0720c5b401767b458034b9435b
Alex Rafter <42029575+Alex-Rafter@users.noreply.github.com>
2022-04-23 15:45:18 +0100

    Initial commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1 class="display-1">{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome to My Page!'
+    };
+  },
+  methods: {
+    // doSomething() {
+    //   alert('Hello!');
+    // }
+  }
+};
+</script>
+<style lang="scss">
+@use "sass:list";
+$colors : red, green, blue;
+
+#app {
+    h1 {
+        color: list.nth($colors,3);
+    }
+}
+
+</style>
